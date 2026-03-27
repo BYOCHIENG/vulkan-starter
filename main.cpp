86551c22811cdf8f6dd88b5392cec700b59df756
Mich Ochieng <mich.ochieng@westfraser.com>
2026-03-27 15:57:41 -0700

    Added code for instantiating a VkInstance

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <stdexcept>
 #include <cstdlib>
+#include <vector>
 
 class VulkanStarter {
 public:
@@ -20,9 +21,7 @@ private:
     const uint32_t WIDTH = 300;
     const uint32_t HEIGHT = 400;
 
-    void initVulkan() {
-
-    }
+    VkInstance instance;
 
     void initWindow() {
         glfwInit();
@@ -30,6 +29,72 @@ private:
         glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
 
         window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan Starter", nullptr, nullptr);
+    }
+
+    void createInstance() {
+        VkApplicationInfo appInfo{};
+
+        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        appInfo.pApplicationName = "Vulkan Starter";
+        appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.pEngineName = "No Engine";
+        appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.apiVersion = VK_API_VERSION_1_0;
+
+        VkInstanceCreateInfo createInfo{};
+
+        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        createInfo.pApplicationInfo = &appInfo;
+
+        uint32_t glfwExtensionCount = 0;
+        const char** glfwExtensions;
+
+        glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+        // MacOS fix for VK_ERROR_INCOMPATIBLE_DRIVER
+        // VK_KHR_PORTABILITY_subset extension is mandatory
+
+        std::vector<const char*> requiredExtensions;
+
+        for(uint32_t i = 0; i < glfwExtensionCount; i++) {
+            requiredExtensions.emplace_back(glfwExtensions[i]);
+        }
+
+        requiredExtensions.emplace_back(VK_KHR_PORTABILITY_ENUMERATION_EXTENSION_NAME);
+
+        createInfo.flags |= VK_INSTANCE_CREATE_ENUMERATE_PORTABILITY_BIT_KHR;
+
+        createInfo.enabledExtensionCount = (uint32_t) requiredExtensions.size();
+        createInfo.ppEnabledExtensionNames = requiredExtensions.data();
+
+        createInfo.enabledLayerCount = 0;
+
+        // Retrieving list of supported extensions - Turn this into function later
+
+        uint32_t extensionCount = 0;
+        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+
+        std::vector<VkExtensionProperties> extensions(extensionCount);
+ 
+        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());
+
+        std::cout << "available extensions:\n";
+
+        for (const auto& extension : extensions) {
+            std::cout << '\t' << extension.extensionName << '\n';
+        }
+
+
+        if (vkCreateInstance(&createInfo, nullptr,&instance) != VK_SUCCESS){
+            throw std::runtime_error("FAILED TO CREATE VK INSTANCE!");
+        } 
+
+        std::printf("Successfully created VK Instance!\n");
+        
+    }
+
+    void initVulkan() {
+        createInstance();
     }
 
     void mainLoop() {
@@ -40,6 +105,8 @@ private:
     }
 
     void cleanup() {
+        vkDestroyInstance(instance,nullptr);
+
         glfwDestroyWindow(window);
 
         glfwTerminate();
