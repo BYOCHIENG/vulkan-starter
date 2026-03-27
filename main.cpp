e913e028fa68fcf2e870a8490cde3cf5213bb28f
Mich Ochieng <mich.ochieng@westfraser.com>
2026-03-27 15:21:11 -0700

    Added base functions for generating window

--- main.cpp
+++ main.cpp
@@ -1,5 +1,62 @@
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+#include <stdexcept>
+#include <cstdlib>
+
+class VulkanStarter {
+public:
+    void run() {
+        initWindow();
+        initVulkan();
+        mainLoop();
+        cleanup();
+    }
+
+private:
+    
+    GLFWwindow* window;
+    const uint32_t WIDTH = 300;
+    const uint32_t HEIGHT = 400;
+
+    void initVulkan() {
+
+    }
+
+    void initWindow() {
+        glfwInit();
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+        window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan Starter", nullptr, nullptr);
+    }
+
+    void mainLoop() {
+        while (!glfwWindowShouldClose(window))
+        {
+            glfwPollEvents();
+        } 
+    }
+
+    void cleanup() {
+        glfwDestroyWindow(window);
+
+        glfwTerminate();
+    }
+
+};
 
 
 int main() {
-    return 0;
+    VulkanStarter app;
+
+    try {
+        app.run();
+    } catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
 }
